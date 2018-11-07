edaac941c350767759c755b80278691031354caa
dxl <du_xiaolong@hotmail.com>
2018-11-07 19:50:29 +0800

    Initial commit

---
+++ app/src/main/java/com/dxl/techreading/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dxl.techreading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @date 2018-11-7 19:49:25
+ * @author du_xi
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
