ad743908de8a150946d25fcd756647f0752f013e
du xiaolong <du_xiaolong@hotmail.com>
2018-12-20 17:02:53 +0800

    透明状态栏

--- app/src/main/java/com/dxl/techreading/base/BaseActivity.java
+++ app/src/main/java/com/dxl/techreading/base/BaseActivity.java
@@ -1,9 +1,14 @@
 package com.dxl.techreading.base;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.dxl.techreading.presenter.IPresenter;
 import com.dxl.techreading.view.IView;
@@ -24,11 +29,26 @@ public abstract class BaseActivity<T extends IPresenter> extends AppCompatActivi
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTranslucentStatus();
         beforeInit();
         initPresenter();
         setContentView(getContentViewLayoutID());
         ButterKnife.bind(this);
         initView();
+    }
+
+    protected void setTranslucentStatus() {
+        // 5.0以上系统状态栏透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 
     /**
@@ -41,7 +61,7 @@ public abstract class BaseActivity<T extends IPresenter> extends AppCompatActivi
         }
     }
 
-    protected T createPresenter(){
+    protected T createPresenter() {
         return null;
     }
 
@@ -54,6 +74,7 @@ public abstract class BaseActivity<T extends IPresenter> extends AppCompatActivi
 
     /**
      * 布局ID
+     *
      * @return .
      */
     protected abstract int getContentViewLayoutID();
@@ -61,7 +82,7 @@ public abstract class BaseActivity<T extends IPresenter> extends AppCompatActivi
     /**
      * 界面初始化
      */
-    protected void initView(){
+    protected void initView() {
 
     }
 
